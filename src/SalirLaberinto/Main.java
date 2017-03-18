38e2febb2494104dfd170ca9f9e8eda3b826bdca
Camila <Camila@USER>
2017-03-18 10:30:06 -0500

    Karel sale de cualquier laberinto

---
+++ src/SalirLaberinto/Main.java
@@ -0,0 +1,20 @@
+package Ejercicio5_1;
+
+public class Main {
+public static void main (String[] args){
+    Nuggets nuggets= new Nuggets();
+    nuggets.A();
+    nuggets.B();
+    nuggets.A();
+    nuggets.C();
+    nuggets.A();
+    nuggets.D();
+    nuggets.B();
+    nuggets.E();
+    nuggets.A();
+    nuggets.F();
+    nuggets.A();
+    nuggets.G();
+    
+}
+}
